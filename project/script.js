53203ea63dd5686064bc63f3531372767effa058
Basireddy vedha <basireddyvedhareddy1973@gmail.com>
2025-12-01 12:37:40 +0530

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,4 @@
+// Simple button click effect on home page
+document.getElementById("exploreBtn")?.addEventListener("click", () => {
+  alert("Welcome! Let’s explore India’s culture and heritage ");
+});
