71f1873375b546af76d22c56e32faabf6b3bbe4c
Basireddy vedha <basireddyvedhareddy1973@gmail.com>
2025-12-01 12:44:35 +0530

    Delete project directory

--- project/script.js
+++
@@ -1,4 +0,0 @@
-// Simple button click effect on home page
-document.getElementById("exploreBtn")?.addEventListener("click", () => {
-  alert("Welcome! Let’s explore India’s culture and heritage ");
-});
